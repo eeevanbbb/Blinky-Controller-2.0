736f283f9cf70303e831c810c8672283840389da
Evan Bernstein <eeevanbbb@me.com>
2015-10-05 14:44:43 -0500

    Initial Commit

---
+++ Blinky Client/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Blinky Client
+//
+//  Created by Evan Bernstein on 10/5/15.
+//  Copyright © 2015 Evan Bernstein. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
